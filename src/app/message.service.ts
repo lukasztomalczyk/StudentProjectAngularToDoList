ce2f84860913f0a9ce5f46b5c6ba0f785efe4a7a
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-10-02 18:56:41 +0200

    refactor

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import { Observable, of } from 'rxjs';
+import {Observable} from 'rxjs';
 import {project} from "./projects-menu/project";
 
 @Injectable({
