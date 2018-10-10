447e5f98239b2630790ba2407053e1d8df0eda73
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-10-10 18:36:06 +0200

    add input

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTaskComponent implements OnInit {
 
-  constructor() { }
+  now: number;
+
+  constructor() {
+    this.now = Date.now();
+  }
 
   ngOnInit() {
   }
-
 }
