162eb746be073b6831f6c6e3ae7cbe8972d01d76
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-10-12 19:54:29 +0200

    work on user nav login

--- src/app/user-nav/user-nav.component.ts
+++ src/app/user-nav/user-nav.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from "../services/auth.service";
+import {AuthService} from '../services/auth.service';
+import {User} from '../services/user';
+import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-user-nav',
@@ -11,12 +14,34 @@ import {AuthService} from "../services/auth.service";
 
 export class UserNavComponent implements OnInit {
 
-  constructor(private auth: AuthService) { }
+  userForm: FormGroup;
+  user: User = null;
+  submitted = false;
+  userLogin = false;
+
+  constructor(private formBuilder: FormBuilder,
+              private authServices: AuthService,
+              private route: Router) { }
 
   ngOnInit() {
+    this.userForm = this.formBuilder.group({
+      name: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    });
   }
 
-  get autorize() {
-    return this.auth.isUserLogin
+  login() {
+    this.submitted = true;
+
+    if (this.userForm.invalid) { return; }
+
+    if (this.authServices.login(this.userForm.controls.name.value,
+        this.userForm.controls.password.value)) {
+        this.userLogin = true;
+      } else {
+        this.userForm.controls.name.setErrors({'nomatch': true});
+        this.userForm.controls.password.setErrors({'nomatch': true});
+        this.userLogin = false;
+      }
   }
 }
